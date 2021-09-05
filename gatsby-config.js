2fe0dfa1cbf09e4bb87d05383a9fe879bac5edc2
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-09-05 18:49:33 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/logo_min_inverted.svg`,
+        icon: `content/assets/logo.svg`,
       },
     },
     `gatsby-plugin-offline`,
